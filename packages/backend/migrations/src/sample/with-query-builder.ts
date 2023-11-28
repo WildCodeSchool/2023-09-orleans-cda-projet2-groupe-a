4390d01a50aee8b1d9e4be343ccdd797cf352bf9
YDrogen <ydrogen45@gmail.com>
2023-11-28 13:59:00 +0100

    fix: eslint, prettier, query builder, precommit

--- packages/backend/migrations/src/sample/with-query-builder.ts
+++ packages/backend/migrations/src/sample/with-query-builder.ts
@@ -5,15 +5,19 @@ import type { Database } from '@app/types';
 
 export async function up(db: Kysely<Database>): Promise<void> {
   // Migration code that update the database to the desired state.
-  await db.schema
-    .createTable('item')
-    .addColumn('id', 'int8', (col) => col.autoIncrement().primaryKey())
-    .addColumn('content', 'text', (col) => col.notNull())
-    .execute();
-  await db.schema.dropTable('item').execute();
+  await db.transaction().execute(async (trx) => {
+    await trx.schema
+      .createTable('item')
+      .addColumn('id', 'int8', (col) => col.autoIncrement().primaryKey())
+      .addColumn('content', 'text', (col) => col.notNull())
+      .execute();
+    await trx.schema.dropTable('item').execute();
+  });
 }
 
 export async function down(db: Kysely<Database>): Promise<void> {
   // Migration code that reverts the database to the previous state.
-  await db.schema.dropTable('item').ifExists().execute();
+  await db.transaction().execute(async (trx) => {
+    await trx.schema.dropTable('item').ifExists().execute();
+  });
 }
