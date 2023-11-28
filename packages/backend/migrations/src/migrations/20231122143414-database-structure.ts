d5463c137c41b768a59422f16c30dc95dbe2f3c4
jaalazard <ja.alazard@proton.me>
2023-11-28 16:31:22 +0100

    update

--- packages/backend/migrations/src/migrations/20231122143414-database-structure.ts
+++ packages/backend/migrations/src/migrations/20231122143414-database-structure.ts
@@ -3,219 +3,248 @@ import { type Kysely, sql } from 'kysely';
 import type { Database } from '@app/types';
 
 export async function up(db: Kysely<Database>): Promise<void> {
-  // Migration code that update the database to the desired state.
-  await sql`
-    CREATE TABLE glass (
-      id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-      name VARCHAR(50) NOT NULL,
-      capacity TINYINT UNSIGNED NOT NULL,
-      image VARCHAR(255) NOT NULL,
-      material ENUM('glass', 'crystal', 'metal', 'plastic', 'wood') NOT NULL
-    );
-  `.execute(db);
+  await db.transaction().execute(async (trx) => {
+    await sql`
+      CREATE TABLE glass (
+        id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(50) NOT NULL,
+        capacity TINYINT UNSIGNED NOT NULL,
+        image VARCHAR(255) NOT NULL,
+        material ENUM('glass', 'crystal', 'metal', 'plastic', 'wood') NOT NULL
+      );
+    `.execute(trx);
+    await sql`
+      DROP TABLE glass;
+    `.execute(trx);
 
-  // Table user
-  await sql`
-    CREATE TABLE user (
-      id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-      email VARCHAR(255) NOT NULL,
-      password VARCHAR(255) NOT NULL,
-      pseudo VARCHAR(50) NOT NULL,
-      image VARCHAR(255) NOT NULL,
-      birthdate VARCHAR(25) NOT NULL,
-      created_at DATETIME NOT NULL
-    );
-  `.execute(db);
+    await sql`
+      CREATE TABLE user (
+        id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        email VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        pseudo VARCHAR(50) NOT NULL,
+        image VARCHAR(255) NOT NULL,
+        birthdate VARCHAR(25) NOT NULL,
+        created_at DATETIME NOT NULL
+      );
+    `.execute(trx);
+    await sql`
+      DROP TABLE user;
+    `.execute(trx);
 
-  // Table ingredient
-  await sql`
-    CREATE TABLE ingredient (
-      id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-      name VARCHAR(50) NOT NULL,
-      color VARCHAR(7) NOT NULL,
-      kcal SMALLINT UNSIGNED NOT NULL,
-      degree TINYINT UNSIGNED NOT NULL,
-      description TEXT NOT NULL,
-      image VARCHAR(255) NOT NULL,
-      flavour ENUM('fruity', 'spicy', 'herbaceous', 'floral', 'woody', 'bitter', 'sweet', 'salty', 'sour', 'neutral') NOT NULL,
-      family ENUM('water', 'alcohol', 'juice', 'soda', 'syrup', 'bitter', 'fruit', 'vegetable', 'herb', 'spice', 'milk', 'cream', 'condiment', 'other') NOT NULL
-    );
-  `.execute(db);
+    await sql`
+      CREATE TABLE ingredient (
+        id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(50) NOT NULL,
+        color VARCHAR(7) NOT NULL,
+        kcal SMALLINT UNSIGNED NOT NULL,
+        degree TINYINT UNSIGNED NOT NULL,
+        description TEXT NOT NULL,
+        image VARCHAR(255) NOT NULL,
+        flavour ENUM('fruity', 'spicy', 'herbaceous', 'floral', 'woody', 'bitter', 'sweet', 'salty', 'sour', 'neutral') NOT NULL,
+        family ENUM('water', 'alcohol', 'juice', 'soda', 'syrup', 'bitter', 'fruit', 'vegetable', 'herb', 'spice', 'milk', 'cream', 'condiment', 'other') NOT NULL
+      );
+    `.execute(trx);
+    await sql`
+      DROP TABLE ingredient;
+    `.execute(trx);
 
-  // Table cocktail
-  await sql`
-    CREATE TABLE cocktail (
-      id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-      name VARCHAR(255) NOT NULL,
-      image VARCHAR(255) NOT NULL,
-      total_kcal TINYINT UNSIGNED NULL NOT NULL,
-      total_degree TINYINT UNSIGNED NOT NULL,
-      author INT UNSIGNED NOT NULL,
-      ratings_average ENUM('0', '0.5', '1', '1.5', '2', '2.5', '3', '3.5', '4', '4.5', '5', '5.5', '6', '6.5', '7', '7.5', '8', '8.5', '9', '9.5', '10') NOT NULL,
-      glass_id INT UNSIGNED NOT NULL,
-      final_flavour ENUM('fruity', 'spicy', 'herbaceous', 'floral', 'woody', 'bitter', 'sweet', 'salty', 'sour', 'neutral') NOT NULL,
-      created_at DATETIME NOT NULL,
-      total_quantity SMALLINT UNSIGNED NOT NULL,
-      FOREIGN KEY (author) REFERENCES user(id),
-      FOREIGN KEY (glass_id) REFERENCES glass(id)
-    );
-  `.execute(db);
+    await sql`
+      CREATE TABLE cocktail (
+        id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        image VARCHAR(255) NOT NULL,
+        total_kcal TINYINT UNSIGNED NULL NOT NULL,
+        total_degree TINYINT UNSIGNED NOT NULL,
+        author INT UNSIGNED NOT NULL,
+        ratings_average ENUM('0', '0.5', '1', '1.5', '2', '2.5', '3', '3.5', '4', '4.5', '5', '5.5', '6', '6.5', '7', '7.5', '8', '8.5', '9', '9.5', '10') NOT NULL,
+        glass_id INT UNSIGNED NOT NULL,
+        final_flavour ENUM('fruity', 'spicy', 'herbaceous', 'floral', 'woody', 'bitter', 'sweet', 'salty', 'sour', 'neutral') NOT NULL,
+        created_at DATETIME NOT NULL,
+        total_quantity SMALLINT UNSIGNED NOT NULL,
+        FOREIGN KEY (author) REFERENCES user(id),
+        FOREIGN KEY (glass_id) REFERENCES glass(id)
+      );
+    `.execute(trx);
+    await sql`
+      DROP TABLE cocktail;
+    `.execute(trx);
 
-  // Table comment
-  await sql`
-    CREATE TABLE comment (
-      id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-      user_id INT UNSIGNED NOT NULL,
-      cocktail_id INT UNSIGNED NOT NULL,
-      content VARCHAR(255) NOT NULL,
-      created_at DATETIME NOT NULL,
-      FOREIGN KEY (user_id) REFERENCES user(id),
-      FOREIGN KEY (cocktail_id) REFERENCES cocktail(id)
-    );
-  `.execute(db);
+    await sql`
+      CREATE TABLE comment (
+        id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        user_id INT UNSIGNED NOT NULL,
+        cocktail_id INT UNSIGNED NOT NULL,
+        content VARCHAR(255) NOT NULL,
+        created_at DATETIME NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES user(id),
+        FOREIGN KEY (cocktail_id) REFERENCES cocktail(id)
+      );
+    `.execute(trx);
+    await sql`
+      DROP TABLE comment;
+    `.execute(trx);
 
-  // Table favorite
-  await sql`
-    CREATE TABLE favorite (
-      id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-      user_id INT UNSIGNED NOT NULL,
-      cocktail_id INT UNSIGNED NOT NULL,
-      FOREIGN KEY (user_id) REFERENCES user(id),
-      FOREIGN KEY (cocktail_id) REFERENCES cocktail(id)
-    );
-  `.execute(db);
+    await sql`
+      CREATE TABLE favorite (
+        id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        user_id INT UNSIGNED NOT NULL,
+        cocktail_id INT UNSIGNED NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES user(id),
+        FOREIGN KEY (cocktail_id) REFERENCES cocktail(id)
+      );
+    `.execute(trx);
+    await sql`
+      DROP TABLE favorite;
+    `.execute(trx);
 
-  // Table rating
-  await sql`
-    CREATE TABLE rating (
-      id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-      user_id INT UNSIGNED NOT NULL,
-      cocktail_id INT UNSIGNED NOT NULL,
-      score ENUM('0', '0.5', '1', '1.5', '2', '2.5', '3', '3.5', '4', '4.5', '5', '5.5', '6', '6.5', '7', '7.5', '8', '8.5', '9', '9.5', '10') NOT NULL,
-      created_at DATETIME NOT NULL,
-      FOREIGN KEY (user_id) REFERENCES user(id),
-      FOREIGN KEY (cocktail_id) REFERENCES cocktail(id)
-    );
-  `.execute(db);
+    await sql`
+      CREATE TABLE rating (
+        id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        user_id INT UNSIGNED NOT NULL,
+        cocktail_id INT UNSIGNED NOT NULL,
+        score ENUM('0', '0.5', '1', '1.5', '2', '2.5', '3', '3.5', '4', '4.5', '5', '5.5', '6', '6.5', '7', '7.5', '8', '8.5', '9', '9.5', '10') NOT NULL,
+        created_at DATETIME NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES user(id),
+        FOREIGN KEY (cocktail_id) REFERENCES cocktail(id)
+      );
+    `.execute(trx);
+    await sql`
+      DROP TABLE rating;
+    `.execute(trx);
 
-  // Table topping
-  await sql`
-    CREATE TABLE topping (
-      id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
-      name VARCHAR(50) NOT NULL
-    );
-  `.execute(db);
+    await sql`
+      CREATE TABLE topping (
+        id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
+        name VARCHAR(50) NOT NULL
+      );
+    `.execute(trx);
+    await sql`
+      DROP TABLE topping;
+    `.execute(trx);
 
-  // Table tool
-  await sql`
-    CREATE TABLE tool (
-      id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-      name VARCHAR(50) NOT NULL,
-      image VARCHAR(255) NOT NULL
-    );
-  `.execute(db);
+    await sql`
+      CREATE TABLE tool (
+        id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(50) NOT NULL,
+        image VARCHAR(255) NOT NULL
+      );
+    `.execute(trx);
+    await sql`
+      DROP TABLE tool;
+    `.execute(trx);
 
-  // Table action
-  await sql`
-    CREATE TABLE action (
-      id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-      verb ENUM('muddle', 'stir', 'shake', 'strain', 'build', 'mix', 'pour', 'garnish', 'twist', 'spritz', 'layer', 'float', 'rim', 'ignite', 'blend', 'top', 'chill', 'heat', 'smoke', 'double strain', 'express', 'infuse', 'dissolve', 'whip', 'squeeze', 'roll', 'dash', 'steam', 'blast chill', 'carbonate', 'mist', 'stir-fry') NOT NULL,
-      priority TINYINT UNSIGNED NOT NULL,
-      tool_id INT UNSIGNED NOT NULL,
-      duration SMALLINT UNSIGNED NOT NULL,
-      complexity TINYINT UNSIGNED NOT NULL,
-      is_mandatory BOOL NOT NULL,
-      FOREIGN KEY (tool_id) REFERENCES tool(id)
-    );
-  `.execute(db);
+    await sql`
+      CREATE TABLE action (
+        id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        verb ENUM('muddle', 'stir', 'shake', 'strain', 'build', 'mix', 'pour', 'garnish', 'twist', 'spritz', 'layer', 'float', 'rim', 'ignite', 'blend', 'top', 'chill', 'heat', 'smoke', 'double strain', 'express', 'infuse', 'dissolve', 'whip', 'squeeze', 'roll', 'dash', 'steam', 'blast chill', 'carbonate', 'mist', 'stir-fry') NOT NULL,
+        priority TINYINT UNSIGNED NOT NULL,
+        tool_id INT UNSIGNED NOT NULL,
+        duration SMALLINT UNSIGNED NOT NULL,
+        complexity TINYINT UNSIGNED NOT NULL,
+        is_mandatory BOOL NOT NULL,
+        FOREIGN KEY (tool_id) REFERENCES tool(id)
+      );
+    `.execute(trx);
+    await sql`
+      DROP TABLE action;
+    `.execute(trx);
 
-  // Table action_ingredient
-  await sql`
-    CREATE TABLE action_ingredient (
-      ingredient_id INT UNSIGNED NOT NULL,
-      action_id INT UNSIGNED NOT NULL,
-      quantity SMALLINT UNSIGNED NOT NULL,
-      FOREIGN KEY (ingredient_id) REFERENCES ingredient(id),
-      FOREIGN KEY (action_id) REFERENCES action(id)
-    );
-  `.execute(db);
+    await sql`
+      CREATE TABLE action_ingredient (
+        ingredient_id INT UNSIGNED NOT NULL,
+        action_id INT UNSIGNED NOT NULL,
+        quantity SMALLINT UNSIGNED NOT NULL,
+        FOREIGN KEY (ingredient_id) REFERENCES ingredient(id),
+        FOREIGN KEY (action_id) REFERENCES action(id)
+      );
+    `.execute(trx);
+    await sql`
+      DROP TABLE action_ingredient;
+    `.execute(trx);
 
-  // Table cocktail_topping
-  await sql`
-    CREATE TABLE cocktail_topping (
-      cocktail_id INT UNSIGNED NOT NULL,
-      topping_id INT UNSIGNED NOT NULL,
-      quantity SMALLINT UNSIGNED NOT NULL,
-      FOREIGN KEY (cocktail_id) REFERENCES cocktail(id),
-      FOREIGN KEY (topping_id) REFERENCES topping(id)
-    );
-  `.execute(db);
+    await sql`
+      CREATE TABLE cocktail_topping (
+        cocktail_id INT UNSIGNED NOT NULL,
+        topping_id INT UNSIGNED NOT NULL,
+        quantity SMALLINT UNSIGNED NOT NULL,
+        FOREIGN KEY (cocktail_id) REFERENCES cocktail(id),
+        FOREIGN KEY (topping_id) REFERENCES topping(id)
+      );
+    `.execute(trx);
+    await sql`
+      DROP TABLE cocktail_topping;
+    `.execute(trx);
 
-  // Table recipe
-  await sql`
-    CREATE TABLE recipe (
-      cocktail_id INT UNSIGNED,
-      action_id INT UNSIGNED NOT NULL,
-      total_complexity TINYINT UNSIGNED NOT NULL,
-      total_duration SMALLINT UNSIGNED NOT NULL,
-      step TINYINT UNSIGNED NOT NULL,
-      FOREIGN KEY (cocktail_id) REFERENCES cocktail(id),
-      FOREIGN KEY (action_id) REFERENCES action(id)
-    );
-  `.execute(db);
+    await sql`
+      CREATE TABLE recipe (
+        cocktail_id INT UNSIGNED,
+        action_id INT UNSIGNED NOT NULL,
+        total_complexity TINYINT UNSIGNED NOT NULL,
+        total_duration SMALLINT UNSIGNED NOT NULL,
+        step TINYINT UNSIGNED NOT NULL,
+        FOREIGN KEY (cocktail_id) REFERENCES cocktail(id),
+        FOREIGN KEY (action_id) REFERENCES action(id)
+      );
+    `.execute(trx);
+    await sql`
+      DROP TABLE recipe;
+    `.execute(trx);
+  });
 }
 
 export async function down(db: Kysely<Database>): Promise<void> {
-  // Migration code that reverts the database to the previous state.
-  await sql`
-    DROP TABLE recipe;
-  `.execute(db);
+  await db.transaction().execute(async (trx) => {
+    await sql`
+      DROP TABLE recipe IF EXISTS;
+    `.execute(trx);
 
-  await sql`
-    DROP TABLE cocktail_topping;
-  `.execute(db);
+    await sql`
+      DROP TABLE cocktail_topping IF EXISTS;
+    `.execute(trx);
 
-  await sql`
-    DROP TABLE action_ingredient;
-  `.execute(db);
+    await sql`
+      DROP TABLE action_ingredient IF EXISTS;
+    `.execute(trx);
 
-  await sql`
-    DROP TABLE action;
-  `.execute(db);
+    await sql`
+      DROP TABLE action IF EXISTS;
+    `.execute(trx);
 
-  await sql`
-    DROP TABLE tool;
-  `.execute(db);
+    await sql`
+      DROP TABLE tool IF EXISTS;
+    `.execute(trx);
 
-  await sql`
-    DROP TABLE topping;
-  `.execute(db);
+    await sql`
+      DROP TABLE topping IF EXISTS;
+    `.execute(trx);
 
-  await sql`
-    DROP TABLE rating;
-  `.execute(db);
+    await sql`
+      DROP TABLE rating IF EXISTS;
+    `.execute(trx);
 
-  await sql`
-    DROP TABLE favorite;
-  `.execute(db);
+    await sql`
+      DROP TABLE favorite IF EXISTS;
+    `.execute(trx);
 
-  await sql`
-    DROP TABLE comment;
-  `.execute(db);
+    await sql`
+      DROP TABLE comment IF EXISTS;
+    `.execute(trx);
 
-  await sql`
-    DROP TABLE cocktail;
-  `.execute(db);
+    await sql`
+      DROP TABLE cocktail IF EXISTS;
+    `.execute(trx);
 
-  await sql`
-    DROP TABLE ingredient;
-  `.execute(db);
+    await sql`
+      DROP TABLE ingredient IF EXISTS;
+    `.execute(trx);
 
-  await sql`
-    DROP TABLE user;
-  `.execute(db);
+    await sql`
+      DROP TABLE user IF EXISTS;
+    `.execute(trx);
 
-  await sql`
-    DROP TABLE glass;
-  `.execute(db);
+    await sql`
+      DROP TABLE glass IF EXISTS;
+    `.execute(trx);
+  });
 }
