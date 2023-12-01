b2b4df224cc5ca8f9da516737fc54aa693479a6c
jaalazard <ja.alazard@proton.me>
2023-12-01 14:51:12 +0100

    changes done

--- packages/backend/migrations/src/migrations/20231201111652-edit-glass-table.ts
+++ packages/backend/migrations/src/migrations/20231201111652-edit-glass-table.ts
@@ -10,3 +10,12 @@ export async function up(db: Kysely<Database>): Promise<void> {
     `.execute(trx);
   });
 }
+
+export async function down(db: Kysely<Database>): Promise<void> {
+  await db.transaction().execute(async (trx) => {
+    await sql`
+      ALTER TABLE glass
+      MODIFY capacity TINYINT UNSIGNED NOT NULL;;
+    `.execute(trx);
+  });
+}
