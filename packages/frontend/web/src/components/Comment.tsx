e6f2e8c8f52bbde5b9e126e68ed9973b181a7a82
Justine Clément-Savary <clement.savary.justine@gmail.com>
2023-12-05 14:14:59 +0100

    fix: review after correction

--- packages/frontend/web/src/components/Comment.tsx
+++ packages/frontend/web/src/components/Comment.tsx
@@ -1,13 +1,15 @@
 import type { CommentProps } from '@app/types';
 
 export default function Comment({ numberComment }: CommentProps) {
-  const comments = Array.from({ length: numberComment }, (_, index) => (
-    <img
-      key={index}
-      src='comment.png'
-      className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4'
-    />
-  ));
-
-  return <div className='flex w-full flex-wrap p-2'>{comments}</div>;
+  return (
+    <div className='flex w-full flex-wrap p-2'>
+      {Array.from({ length: numberComment }, (_, index) => (
+        <img
+          key={index}
+          src='comment.png'
+          className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4'
+        />
+      ))}
+    </div>
+  );
 }
--- packages/frontend/web/src/components/StarRating.tsx
+++ packages/frontend/web/src/components/StarRating.tsx
@@ -3,10 +3,11 @@ import type { StarRatingProps } from '@app/types';
 import Star from './Star';
 
 export default function StarRating({ starCount }: StarRatingProps) {
-  const stars = [];
-  for (let index = 0; index < starCount; index++) {
-    stars.push(<Star key={index} index={index} />);
-  }
-
-  return <div className='mt-2 flex justify-center'>{stars}</div>;
+  return (
+    <div className='mt-2 flex justify-center'>
+      {Array.from({ length: starCount }, (_, index) => (
+        <Star key={index} index={index} />
+      ))}
+    </div>
+  );
 }
