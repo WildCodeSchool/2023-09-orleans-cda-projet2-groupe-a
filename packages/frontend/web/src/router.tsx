0c200173538cba9563833d063111e28603b66658
Cardamced <120644423+Cardamced@users.noreply.github.com>
2023-12-06 10:24:53 +0100

    feat: auth fronted
    Add login & register pages & routes.
    
    Rebase auth frontend

--- packages/frontend/web/src/router.tsx
+++ packages/frontend/web/src/router.tsx
@@ -4,6 +4,7 @@ import AddCocktail from './pages/AddCocktail';
 import CocktailsDetails from './pages/CocktailsDetails';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 const router = createBrowserRouter([
   {
@@ -12,6 +13,18 @@ const router = createBrowserRouter([
   },
   {
     path: '/details/:id',
+    element: <CocktailsDetails />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/details',
     element: <CocktailsDetails />,
   },
   {
