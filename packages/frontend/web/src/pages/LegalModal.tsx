f85753525db551f5c70fdfc48b4830c6f6802659
Cardamced <120644423+Cardamced@users.noreply.github.com>
2023-12-14 16:09:41 +0100

    feat: legal-notice
    Add Modal

--- packages/frontend/web/src/pages/LegalModal.tsx
+++ packages/frontend/web/src/pages/LegalModal.tsx
@@ -1,29 +1,32 @@
 import { useState } from 'react';
 
 export default function LegalNotice() {
-  const [isModalShown, setIsModalShown] = useState(false);
+  const [isModalShown, setIsModalShown] = useState(true);
 
-  return isModalShown ? (
-    <div className='flex-end font-stroke text-light flex items-center'>
-      <div className='z-40 flex h-1/6 flex-col items-center gap-6 text-4xl'>
-        <div className='hover:text-dark-orange hover:bg-light-yellow m-2 rounded-[30px] border-black p-4 text-6xl font-bold transition-transform  duration-500 ease-in-out hover:rotate-1 hover:scale-110 hover:justify-normal hover:border-[5px] hover:bg-opacity-80'>
-          <a href='/virgin'>{'Legal Notice'}</a>
+  return (
+    isModalShown ? (
+      <div className='flex-end font-stroke text-light flex items-center'>
+        <div className='z-40 flex h-screen w-screen flex-col items-center gap-6 text-4xl'>
+          <div className='hover:text-pastel-blue hover:bg-light-green m-2 rounded-[30px] border-black p-4 text-6xl font-bold transition-transform  duration-500 ease-in-out hover:rotate-1 hover:scale-110 hover:justify-normal hover:border-[5px] hover:bg-opacity-80'>
+            <a href='/virgin'>{'Legal Notice'}</a>
+          </div>
+          <div
+            className='bg-dark-blue shadow-2x1 z-50 h-1/3 w-1/3 justify-center rounded-[90px] border-[6px] border-black object-center opacity-100 shadow-inner'
+          />
         </div>
-        <img
-          className='shadow-2x1 z-50 h-2/3 w-2/3 justify-center rounded-[90px] border-[6px] border-black object-center opacity-100 shadow-inner'
-          src='./alcohol-free-cocktails.webp'
-          alt='redirection page for under18'
-        />
         <button
           type='button'
-          className='font-stroke text-light hover:text-dark-orange duration-250 flex h-[10px] w-[10px] justify-end transition-transform ease-in-out hover:scale-110'
+          className='font-stroke text-light hover:text-dark-blue duration-250 flex h-[10px] w-[10px] justify-end transition-transform ease-in-out hover:scale-110'
           onClick={() => {
             setIsModalShown(false);
           }}
         >
-          <div className=''>{'X'}</div>
+          <div className='items-center'>{'X'}</div>
         </button>
       </div>
-    </div>
-  ) : null;
+    ) : (
+    <div className="h-1/2 w-1/2 font-stroke text-light text-xl">Coucou</div>
+  )
+    );
+  
 }
