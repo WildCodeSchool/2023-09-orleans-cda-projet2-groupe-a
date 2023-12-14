0193de20c0216f26beae3d6f6233c7b31b671bf4
Cardamced <120644423+Cardamced@users.noreply.github.com>
2023-12-14 12:50:16 +0100

    feat: auth-frontend
    Fixed Provider call & comments.

--- packages/frontend/web/src/main.tsx
+++ packages/frontend/web/src/main.tsx
@@ -9,6 +9,9 @@ import router from './router.tsx';
 
 const rootElement = document.querySelector('#root');
 
+// Taken into account that CheckBirthdate can come before authentication,
+// AgeProvider will be placed around AuthProvider.
+// If Authentication came before, AgeProvider should be placed inside of AuthProvider.
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
--- packages/frontend/web/src/pages/CheckBirthdate.tsx
+++ packages/frontend/web/src/pages/CheckBirthdate.tsx
@@ -1,7 +1,6 @@
 import { type FormEvent, useEffect, useMemo, useState } from 'react';
 import { Navigate, useNavigate } from 'react-router-dom';
 
-import { CurrentAgeContext } from '@/contexts/AgeContext';
 import { useAuth } from '@/contexts/AuthContext';
 
 export default function CheckBirthdate() {
@@ -96,94 +95,92 @@ export default function CheckBirthdate() {
   }
 
   return (
-    <CurrentAgeContext.Provider value={value}>
-      <div className='bg-pastel-blue flex h-screen items-center justify-center p-5'>
-        <div
-          className='absolute h-screen w-screen overflow-x-hidden bg-center bg-no-repeat'
-          style={{ backgroundImage: `url('/enter.svg')` }}
-        >
-          <div className='flex h-screen w-screen flex-col items-center justify-center'>
-            <h1 className='text-light font-stroke mb-4 mt-12 pb-6 text-center text-5xl font-bold leading-normal'>
-              {'Enter your birthdate'}
-            </h1>
-            <form
-              onSubmit={handleSubmit}
-              className='m-4 flex flex-col items-center justify-center pt-[.5rem]'
+    <div className='bg-pastel-blue flex h-screen items-center justify-center p-5'>
+      <div
+        className='absolute h-screen w-screen overflow-x-hidden bg-center bg-no-repeat'
+        style={{ backgroundImage: `url('/enter.svg')` }}
+      >
+        <div className='flex h-screen w-screen flex-col items-center justify-center'>
+          <h1 className='text-light font-stroke mb-4 mt-12 pb-6 text-center text-5xl font-bold leading-normal'>
+            {'Enter your birthdate'}
+          </h1>
+          <form
+            onSubmit={handleSubmit}
+            className='m-4 flex flex-col items-center justify-center pt-[.5rem]'
+          >
+            <input
+              className='2px border-dark h-18 z-40 m-1 h-16 w-72 rounded border-[5px] p-1 text-center text-xl md:w-80 md:text-3xl'
+              type='birthdate'
+              placeholder='Birthdate'
+              value={birthdate}
+              onChange={(event) => {
+                setBirthdate(event.target.value);
+              }}
+              maxLength={10}
+            />
+            <button
+              className='button border-dark mt-3 h-14 w-[288px] rounded border-[5px] bg-blue-500 p-1 text-lg font-bold text-white hover:bg-blue-700 md:w-[320px] md:text-xl'
+              type='submit'
             >
-              <input
-                className='2px border-dark h-18 z-40 m-1 h-16 w-72 rounded border-[5px] p-1 text-center text-xl md:w-80 md:text-3xl'
-                type='birthdate'
-                placeholder='Birthdate'
-                value={birthdate}
-                onChange={(event) => {
-                  setBirthdate(event.target.value);
-                }}
-                maxLength={10}
-              />
-              <button
-                className='button border-dark mt-3 h-14 w-[288px] rounded border-[5px] bg-blue-500 p-1 text-lg font-bold text-white hover:bg-blue-700 md:w-[320px] md:text-xl'
-                type='submit'
-              >
-                {'Register'}
-              </button>
-            </form>
-          </div>
+              {'Register'}
+            </button>
+          </form>
         </div>
-        <div className='fixed top-1 z-40 flex h-1/5 flex-col items-center justify-center'>
-          {isSubmitted && birthdate !== '' ? (
-            <>
-              {!/^(19|20)\d{2}[./-\s](0[1-9]|1[0-2])[./-\s](0[1-9]|[12]\d|3[01])$/.test(
-                birthdate,
-              ) && (
-                <div className='fixed top-1 rounded border-2 border-red-600 bg-red-300 p-1'>
-                  {'Birthdate format : YYYY MM DD'}
-                </div>
-              )}
-              {isUnder18 ? (
-                <div className='w-7/8 h-7/8 fixed top-12 flex rounded border-2 border-red-600 bg-red-300 p-1'>
-                  {'Sorry! You must be at least 18 years old'}
-                </div>
-              ) : null}
-            </>
-          ) : null}
-        </div>
-        <div className='z-50'>
-          {isImageShown && isSubmitted && birthdate !== '' ? (
-            <div
-              className={`w-7/8 animate-fade-out ml-[5rem] flex h-screen items-center justify-center`}
-            >
-              <img
-                src='/ouch.svg'
-                alt='ouch! Not Eighteen.'
-                className='w-7/8 h-7/8 ml-[5rem]'
-              />
-            </div>
-          ) : null}
-        </div>
-        {isModalShown ? (
-          <div className='flex-end font-stroke text-light flex items-center'>
-            <div className='z-40 flex h-1/6 flex-col items-center gap-6 text-4xl'>
-              <div className='hover:text-dark-orange hover:bg-light-yellow m-2 rounded-[30px] border-black p-4 text-6xl font-bold transition-transform  duration-500 ease-in-out hover:rotate-1 hover:scale-110 hover:justify-normal hover:border-[5px] hover:bg-opacity-80'>
-                <a href='/virgin'>{'Grab your Mocktail!'}</a>
+      </div>
+      <div className='fixed top-1 z-40 flex h-1/5 flex-col items-center justify-center'>
+        {isSubmitted && birthdate !== '' ? (
+          <>
+            {!/^(19|20)\d{2}[./-\s](0[1-9]|1[0-2])[./-\s](0[1-9]|[12]\d|3[01])$/.test(
+              birthdate,
+            ) && (
+              <div className='fixed top-1 rounded border-2 border-red-600 bg-red-300 p-1'>
+                {'Birthdate format : YYYY MM DD'}
               </div>
-              <img
-                className='shadow-2x1 z-50 h-2/3 w-2/3 justify-center rounded-[90px] border-[6px] border-black object-center opacity-100 shadow-inner'
-                src='./alcohol-free-cocktails.webp'
-                alt='redirection page for under18'
-              />
-              <button
-                type='button'
-                className='font-stroke text-light hover:text-dark-orange duration-250 flex h-[10px] w-[10px] justify-end transition-transform ease-in-out hover:scale-110'
-                onClick={() => {
-                  setIsModalShown(false);
-                }}
-              >
-                <div className=''>{'X'}</div>
-              </button>
-            </div>
+            )}
+            {isUnder18 ? (
+              <div className='w-7/8 h-7/8 fixed top-12 flex rounded border-2 border-red-600 bg-red-300 p-1'>
+                {'Sorry! You must be at least 18 years old'}
+              </div>
+            ) : null}
+          </>
+        ) : null}
+      </div>
+      <div className='z-50'>
+        {isImageShown && isSubmitted && birthdate !== '' ? (
+          <div
+            className={`w-7/8 animate-fade-out ml-[5rem] flex h-screen items-center justify-center`}
+          >
+            <img
+              src='/ouch.svg'
+              alt='ouch! Not Eighteen.'
+              className='w-7/8 h-7/8 ml-[5rem]'
+            />
           </div>
         ) : null}
       </div>
-    </CurrentAgeContext.Provider>
+      {isModalShown ? (
+        <div className='flex-end font-stroke text-light flex items-center'>
+          <div className='z-40 flex h-1/6 flex-col items-center gap-6 text-4xl'>
+            <div className='hover:text-dark-orange hover:bg-light-yellow m-2 rounded-[30px] border-black p-4 text-6xl font-bold transition-transform  duration-500 ease-in-out hover:rotate-1 hover:scale-110 hover:justify-normal hover:border-[5px] hover:bg-opacity-80'>
+              <a href='/virgin'>{'Grab your Mocktail!'}</a>
+            </div>
+            <img
+              className='shadow-2x1 z-50 h-2/3 w-2/3 justify-center rounded-[90px] border-[6px] border-black object-center opacity-100 shadow-inner'
+              src='./alcohol-free-cocktails.webp'
+              alt='redirection page for under18'
+            />
+            <button
+              type='button'
+              className='font-stroke text-light hover:text-dark-orange duration-250 flex h-[10px] w-[10px] justify-end transition-transform ease-in-out hover:scale-110'
+              onClick={() => {
+                setIsModalShown(false);
+              }}
+            >
+              <div className=''>{'X'}</div>
+            </button>
+          </div>
+        </div>
+      ) : null}
+    </div>
   );
 }
