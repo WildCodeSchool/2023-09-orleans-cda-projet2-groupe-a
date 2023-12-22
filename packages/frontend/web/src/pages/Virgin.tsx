e500d461485312b9d35a6d1b4b5566a0175d58ee
Cardamced <120644423+Cardamced@users.noreply.github.com>
2023-12-22 15:58:49 +0100

    feat: virgin
    add Virgin page.

--- packages/frontend/web/src/pages/Virgin.tsx
+++ packages/frontend/web/src/pages/Virgin.tsx
@@ -4,8 +4,10 @@ export default function Virgin() {
       className='z-20 flex h-screen w-screen items-start justify-center bg-cover p-5'
       style={{ backgroundImage: `url('/bg-virgin.svg')` }}
     >
-      <div className='mx-auto flex h-[13rem] w-[90vw] bg-card-virgin-orange border-4 border-light-orange rounded-lg p-3 shadow-2xl sm:w-[70vw] md:h-[13rem] md:w-[27rem]'>
-        <h1 className='font-stroke text-light absolute z-50 m-auto flex stroke-[2rem] py-[4rem] text-4xl ps-5 text-center text-[2.5rem] font-extrabold uppercase'>{'Virgin'}</h1>
+      <div className='bg-card-virgin-orange border-light-orange mx-auto flex h-[13rem] w-[90vw] rounded-lg border-4 p-3 shadow-2xl sm:w-[70vw] md:h-[13rem] md:w-[27rem]'>
+        <h1 className='font-stroke text-light absolute z-50 m-auto flex stroke-[2rem] py-[4rem] ps-5 text-center text-4xl text-[2.5rem] font-extrabold uppercase'>
+          {'Virgin'}
+        </h1>
         <div
           className='z-50 h-[200px] w-[350px] bg-center bg-no-repeat'
           style={{ backgroundImage: `url('/feeding-bottle-cocktails-2.svg')` }}
