6d89e3d308ed07f98694ebfcaa035f02354fb782
No3ml3 <noemie.barre24@gmail.com>
2024-01-03 16:01:22 +0100

    fix

--- packages/frontend/web/src/components/form-cocktail/IngredientToChoose.tsx
+++ packages/frontend/web/src/components/form-cocktail/IngredientToChoose.tsx
@@ -58,7 +58,7 @@ export default function IngredientToChoose({
     </ul>
   ) : (
     <>
-      <fieldset className=' relative bottom-[7%] right-[-7%] grid w-[200px] grid-flow-col grid-rows-3 gap-2 gap-x-2 sm:bottom-[10%] sm:right-[-13%] sm:w-[300px]'>
+      <fieldset className='relative bottom-[7%] right-[-7%] grid h-[88px] w-[200px] grid-flow-col grid-rows-3 gap-2 gap-x-2 sm:bottom-[10%] sm:right-[-13%] sm:w-[300px]'>
         {isLoading
           ? undefined
           : data?.map((ingredient) => (
