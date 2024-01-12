879ce4842bb061724455f26bf46fe8b6ecc53228
Cardamced <120644423+Cardamced@users.noreply.github.com>
2024-01-12 16:15:54 +0100

    feat: virgin
    Worrrrk in progress

--- packages/backend/api/src/virgin.ts
+++ packages/backend/api/src/virgin.ts
@@ -5,12 +5,13 @@ import { db } from '@app/backend-shared';
 const virginRouter = express.Router();
 
 // Route get to retrieve virgin cocktails where total_degree equals 0
-virginRouter.get('/', async (req, res) => {
+virginRouter.get('/virgin', async (req, res) => {
   try {
     const virginCocktails = await db
       .selectFrom('cocktail')
       .selectAll()
       .where('total_degree', '=', 0)
+      // jointure avec la table Ingrédients.family est différent de alcool. (Cf. Justine)
       .execute();
 
     res.json({ virginCocktails });
@@ -19,5 +20,7 @@ virginRouter.get('/', async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 });
+
+
 
 export { virginRouter };
--- packages/frontend/web/src/pages/Virgin.tsx
+++ packages/frontend/web/src/pages/Virgin.tsx
@@ -1,59 +1,54 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
+import CocktailCard from '@/components/cocktail-detail/CocktailCard';
+import{ useState } from 'react';
+
+type Cocktail = {
+  cocktail: Cocktail;
+  name: string;
+  description: string;
+  // Ajoutez ici d'autres propriétés si nécessaire
+};
 
 export default function VirginCocktails() {
-  const [cocktails, setCocktails] = useState([]);
+  const [cocktails, setCocktails] = useState<Cocktail[]>([]);
 
-  type Cocktail = {
-    name: string;
-    description: string;
-    // Ajoutez ici d'autres propriétés si nécessaire
-  };
 
-  useEffect(() => {
-    const fetchCocktails = async (): Promise<void> => {
-      try {
-        const response = await fetch('/virgin'); // Remplacez ceci par l'URL de votre API
-        const data = await response.json();
-        setCocktails(data.virginCocktails);
-      } catch (error) {
-        console.error('Failed to fetch cocktails:', error);
-      }
-    };
+  // fetch ici le virgin.ts
 
-    fetchCocktails().catch((error) => {
-      console.error('Failed to fetch cocktails:', error);
-    });
-  }, []);
+  // const fetchVirgin async function 
+  
+  // (params: type) {
+    
+
+  // }
+  // const response await fetch Virgin('/virgin');
+
+  
+
 
   return (
-    <>
-      <div>
-        <h1>{'Virgin Cocktails'}</h1>
-        {cocktails.map((cocktail) => (
-          <div key={cocktail}>
-            {/* <h2>{cocktail.name}</h2> */}
-            <p>{'cocktail.description'}</p>
-            {/* Ajoutez ici d'autres détails sur le cocktail */}
-          </div>
-        ))}
-      </div>
-      <div
-        className='z-20 flex h-screen w-screen items-start justify-center bg-cover p-5'
-        style={{ backgroundImage: `url('/bg-virgin.svg')` }}
-      >
-        <div className='bg-card-virgin-orange border-light-orange mx-auto flex h-[13rem] w-[90vw] rounded-lg border-4 p-3 shadow-2xl sm:w-[70vw] md:h-[13rem] md:w-[27rem]'>
-          <h1 className='font-stroke text-light absolute z-50 m-auto flex stroke-[2rem] py-[4rem] ps-5 text-center text-4xl text-[2.5rem] font-extrabold uppercase'>
-            {'Virgin'}
-          </h1>
-          <div
-            className='z-50 h-[200px] w-[350px] bg-center bg-no-repeat'
-            style={{
-              backgroundImage: `url('/feeding-bottle-cocktails-2.svg')`,
-            }}
-          />
+    <div
+      className='z-20 flex h-screen w-screen items-start justify-center bg-cover p-5'
+      style={{ backgroundImage: `url('/bg-virgin.svg')` }}
+    >
+      <div className='bg-card-virgin-orange border-light-orange mx-auto flex h-[13rem] w-[90vw] rounded-lg border-4 p-3 shadow-2xl sm:w-[70vw] md:h-[13rem] md:w-[27rem]'>
+        <h1 className='font-stroke text-light absolute z-50 m-auto flex stroke-[2rem] py-[4rem] ps-5 text-center text-4xl text-[2.5rem] font-extrabold uppercase'>
+          {'Virgin'}
+        </h1>
+        <div
+          className='z-50 h-[200px] w-[350px] bg-center bg-no-repeat'
+          style={{
+            backgroundImage: `url('/feeding-bottle-cocktails-2.svg')`,
+          }}
+        />
+        <div className="flex-1 items-start">
+          <CocktailCard />
         </div>
+        {/* <div className="justify-items">
+          {cocktails.map((cocktail) => (
+            <CocktailCard key={cocktail.name} cocktail={cocktail} />
+          ))}
+        </div> */}
       </div>
-    </>
+    </div>
   );
 }
