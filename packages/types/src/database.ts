24a0dab1d7b0787091bbfea4323ae9967ef362ca
jaalazard <ja.alazard@proton.me>
2024-01-17 15:17:11 +0100

    database structure & types

--- packages/types/src/database.ts
+++ packages/types/src/database.ts
@@ -1,14 +1,68 @@
-import type { Generated, Insertable, Selectable, Updateable } from 'kysely';
-
-export interface ItemTable {
-  id: Generated<number>;
-  content: string;
+export interface Product {
+  id: number;
+  name: string;
+  brand: string;
+  category: string;
+  image: string | null;
 }
 
-export type Item = Selectable<ItemTable>;
-export type NewItem = Insertable<ItemTable>;
-export type ItemUpdate = Updateable<ItemTable>;
+export interface Criteria {
+  id: number;
+  name: string;
+}
 
+export interface CriteriaValue {
+  id: number;
+  criteria_id: number;
+  value: string;
+}
+
+export interface Consumer {
+  id: number;
+  email: string;
+  password: string;
+  firstname: string;
+  lastname: string;
+  birthdate: Date;
+  gender: 'male' | 'female' | 'other';
+  country: string;
+  created_at: Date;
+}
+
+export interface Command {
+  id: number;
+  created_at: Date;
+  consumer_id: number;
+  totalAmount: number;
+  totalQuantity: number;
+  status: 'pending' | 'completed' | 'cancelled';
+}
+
+export interface CommandLine {
+  id: number;
+  command_id: number;
+  product_id: number;
+  quantity: number;
+  unity_price: number;
+  total_price: number;
+}
+
+export interface ConsumerValue {
+  consumer_id: number;
+  criteria_value_id: number;
+}
+
+export interface ProductValue {
+  product_id: number;
+  criteria_value_id: number;
+}
 export interface Database {
-  item: ItemTable;
+  product: Product;
+  criteria: Criteria;
+  criteria_value: CriteriaValue;
+  consumer: Consumer;
+  command: Command;
+  command_line: CommandLine;
+  consumer_value: ConsumerValue;
+  product_value: ProductValue;
 }
