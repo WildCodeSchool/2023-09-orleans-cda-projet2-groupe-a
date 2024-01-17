03c41830d7029babae0c5049c8d9bb5dfe38ce67
Justine Clément-Savary <clement.savary.justine@gmail.com>
2024-01-17 23:58:58 +0100

    navigation menu ok

--- packages/frontend/web/src/router.tsx
+++ packages/frontend/web/src/router.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom';
 
+import Diagnostic from './pages/Diagnostic';
 import Home from './pages/Home';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
+  },
+  {
+    path: '/diagnostic',
+    element: <Diagnostic />,
   },
 ]);
 
