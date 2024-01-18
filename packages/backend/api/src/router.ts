77b3a15d36a4ffe27c0c20f059bfcc007c55efbc
greegouze <gregory.silvestri45@gmail.com>
2024-01-18 14:33:53 +0100

    form back

--- packages/backend/api/src/router.ts
+++ packages/backend/api/src/router.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import authRouter from './auth/auth';
+import { criteriaRouter } from './criteria/criteria-crud';
 
 const router = express.Router();
 
 router.use('/auth', authRouter);
+router.use('/criteria', criteriaRouter);
 
 export default router;
