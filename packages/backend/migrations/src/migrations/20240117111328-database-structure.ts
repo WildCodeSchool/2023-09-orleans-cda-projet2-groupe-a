f16e17861f97ab05f04c29b3f6ecaa88a3616970
jaalazard <ja.alazard@proton.me>
2024-01-18 10:34:05 +0100

    adding a price field to product's table

--- packages/backend/migrations/src/migrations/20240117111328-database-structure.ts
+++ packages/backend/migrations/src/migrations/20240117111328-database-structure.ts
@@ -11,8 +11,8 @@ export async function up(db: Kysely<Database>): Promise<void> {
         name VARCHAR(255) NOT NULL,
         brand VARCHAR(255) NOT NULL,
         category VARCHAR(255) NOT NULL,
-        image VARCHAR(255)
-        price DECIMAL(10, 2) NOT NULL,
+        image VARCHAR(255),
+        price DECIMAL(10, 2) NOT NULL
       );
     `.execute(trx);
 
