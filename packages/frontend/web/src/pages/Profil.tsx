c05c90992b14563db5bf40bd76b82655a98e4723
Abecker37 <72383676+Abecker37@users.noreply.github.com>
2024-01-19 09:51:02 +0100

    profil-2

--- packages/frontend/web/src/pages/Profil.tsx
+++ packages/frontend/web/src/pages/Profil.tsx
@@ -15,13 +15,14 @@ export default function Profil() {
             <img src='/Logo2.svg' alt='logo' className='w-full' />
           </div>
           <div className='bg-secondary text-primary font-base flex grow flex-row items-center justify-center gap-6 p-3'>
-            <form className='flex flex-col gap-6'>
+            <form  className='flex flex-col gap-6'>
               <div className='flex flex-col gap-2'>
                 <label htmlFor='prenom'>{"Prénom:"}</label>
                 <input
                   type='text'
                   id='prenom'
-                  value='Amaury'
+                  value='Mike'
+                  
                   className='border-divider text-primary w-full border-b-2 bg-transparent'
                 />
               </div>
@@ -30,7 +31,7 @@ export default function Profil() {
                 <input
                   type='text'
                   id='nom'
-                  value='Becker'
+                  value='Xiong'
                   className='border-divider text-primary w-full border-b-2 bg-transparent'
                 />
               </div>
@@ -39,29 +40,22 @@ export default function Profil() {
                 <input
                   type='text'
                   id='mail'
-                  value='toto@gmail.com'
+                  value='Mike@test.com'
                   className='border-divider text-primary w-full border-b-2 bg-transparent'
                 />
               </div>
               <div className='flex flex-col gap-2'>
                 <label htmlFor='birthdate'>{"Date de naissance:"}</label>
-                <input
-                  type='date'
-                  id='birthdate'
-                  className='border-divider bg-transparent'
-                />
+                <p className='border-divider bg-transparent'>07/09/1993</p>
               </div>
               <div className='flex flex-col gap-2'>
                 <label htmlFor='genre'>{"Genre:"}</label>
-                <select id='genre' className='border-divider bg-transparent'>
+                <select id='genre' value="male" className='border-divider bg-transparent'>
                   <option value='male'>{"Homme"}</option>
                   <option value='female'>{"Femme"}</option>
                   <option value='other'>{"Autre"}</option>
                 </select>
               </div>
-              <button className='bg-primary text-secondary rounded-sm'>
-                {"Modifier"}
-              </button>
             </form>
           </div>
         </div>
