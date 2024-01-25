0d8d66eb5dfb304511a43da516cf4fc78f5cbaf0
Cardamced <120644423+Cardamced@users.noreply.github.com>
2024-01-25 12:09:15 +0100

    feat: virgin
    Fixed sql request & style

--- packages/frontend/web/src/components/cocktail-detail/VirginCocktailCard.tsx
+++ packages/frontend/web/src/components/cocktail-detail/VirginCocktailCard.tsx
@@ -1,36 +1,59 @@
+import { Link } from 'react-router-dom';
+
 import type { VirginCocktail } from '@/pages/Virgin';
 
 type CocktailCardProps = {
   readonly virginCocktail: VirginCocktail;
   readonly name: string;
-  readonly description: string;
+  readonly avg_rating: number;
+  readonly cocktail_id: number;
+  readonly virginCocktails: VirginCocktail[] | undefined;
 };
 export default function VirginCocktailCard({
-  virginCocktail,
   name,
-  description,
+  avg_rating,
+  virginCocktail,
 }: CocktailCardProps) {
-  console.log(virginCocktail.description);
-  console.log(virginCocktail);
+  if (!virginCocktail) {
+    return null;
+  }
+
   return (
     <div className='relative -top-8'>
-      <div className='border-dark bg-card-pink absolute -top-3 left-10 z-30 m-auto my-20 h-[21rem] w-[18rem] rounded-sm border-[3px]' />
-      <div className='border-dark bg-pastel-brown absolute -top-6 left-6 m-auto my-20 h-[21rem] w-[18rem] rounded-sm border-[3px]' />
-      <div className='border-dark bg-card-pink-dark absolute left-14 z-[50] my-20 h-[21rem] w-[18rem] rounded-sm border-[3px] uppercase'>
-        <img
-          src='/cocktail-placeholder.png'
-          alt='Cocktail picture'
-          className='border-dark mx-auto mt-8 h-[13rem] w-[14rem] rounded-sm border-[3px] object-cover'
-        />
-        <div>
-          <p className='font-stroke text-light text-md mx-4 mt-3 text-center'>
-            {name}
-          </p>
-          <p className='py-2 pl-3 text-sm'>{description}</p>
-          <p>{virginCocktail.description}</p>
-          <div className='mt-2 flex justify-center' />
+      <Link to={`/details/${virginCocktail.id}`}>
+        <div className='border-dark bg-card-pink absolute -top-3 left-10 z-30 m-auto my-20 h-[21rem] w-[18rem] rounded-sm border-[3px]' />
+        <div className='border-dark bg-pastel-brown absolute -top-6 left-6 m-auto my-20 h-[21rem] w-[18rem] rounded-sm border-[3px]' />
+        <div className='border-dark bg-card-pink-dark absolute left-14 z-[50] my-20 h-[21rem] w-[18rem] rounded-sm border-[3px] normal-case'>
+          <img
+            src='/cocktail-placeholder.png'
+            alt='Cocktail picture'
+            className='border-dark mx-auto mt-8 h-[13rem] w-[14rem] rounded-sm border-[3px] object-cover'
+          />
+          <div>
+            <p className='font-stroke text-light text-md mx-4 mt-3 text-center'>
+              {name}
+            </p>
+            {/* <p className='py-2 pl-3 text-sm'>{'coucou'}</p> */}
+            <div className='flex justify-center py-3'>
+              {avg_rating === 0 ? (
+                <p className='text-sm font-extralight'>{'not grade yet'}</p>
+              ) : (
+                [1, 2, 3, 4, 5].map((index) => (
+                  <div
+                    key={index}
+                    className={`h-[30px] w-[30px] bg-[url('/star-yellow.png')] bg-cover bg-no-repeat ${
+                      index <= Math.floor(avg_rating / 2)
+                        ? 'grayscale-0'
+                        : 'grayscale'
+                    }`}
+                  />
+                ))
+              )}
+            </div>
+            <div className='mt-2 flex justify-center' />
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
--- packages/frontend/web/src/pages/Virgin.tsx
+++ packages/frontend/web/src/pages/Virgin.tsx
@@ -5,10 +5,11 @@ import VirginCocktailCard from '@/components/cocktail-detail/VirginCocktailCard'
 export type VirginCocktail = {
   id: number;
   virginCocktail: VirginCocktail;
+  virginCocktails: VirginCocktail[] | undefined;
+  cocktail_id: number;
   name: string;
-  description: string;
+  avg_rating: number;
 };
-
 export default function VirginCocktails() {
   const [virginCocktails, setVirginCocktails] = useState<VirginCocktail[]>([]);
 
@@ -38,16 +39,20 @@ export default function VirginCocktails() {
       className='z-20 min-h-screen w-screen grid-flow-row bg-cover p-5'
       style={{ backgroundImage: `url('/bg-virgin.svg')` }}
     >
-      <div className='bg-card-virgin-orange border-light-orange flex h-full w-full justify-center rounded-lg border-4 p-3 shadow-2xl sm:w-[70vw] md:h-[13rem] md:w-[27rem]'>
-        <h1 className='font-stroke text-light absolute z-50 m-auto flex stroke-[2rem] py-[4rem] ps-5 text-center text-4xl text-[2.5rem] font-extrabold uppercase'>
-          {'Virgin'}
-        </h1>
-        <div
-          className='z-50 h-[200px] w-[350px] bg-center bg-no-repeat'
-          style={{
-            backgroundImage: `url('/feeding-bottle-cocktails-2.svg')`,
-          }}
-        />
+      <div className='flex h-full w-full items-center justify-center'>
+        <div className='bg-card-virgin-orange border-light-orange flex h-full w-full justify-center rounded-lg border-4 p-3 shadow-2xl sm:w-[70vw] md:h-[13rem] md:w-[27rem]'>
+          <div className='flex h-full w-full flex-col items-center justify-center'>
+            <h1 className='font-stroke text-light absolute z-50 m-auto mr-[200px] flex stroke-[2rem] py-[4rem] ps-5 text-center text-4xl text-[2.5rem] font-extrabold uppercase'>
+              {'Virgin'}
+            </h1>
+            <div
+              className='z-50 ml-[200px] h-[200px] w-[350px] bg-center bg-no-repeat'
+              style={{
+                backgroundImage: `url('/feeding-bottle-cocktails-2.svg')`,
+              }}
+            />
+          </div>
+        </div>
       </div>
       <div className='px-10'>
         <div className=' flex flex-col items-center justify-center gap-y-10 md:my-5 md:grid md:grid-cols-2 md:flex-row lg:grid-cols-3 xl:grid-cols-4 2xl:px-20'>
@@ -56,8 +61,10 @@ export default function VirginCocktails() {
               <div key={index} className='pb-[22rem]'>
                 <VirginCocktailCard
                   name={cocktail.name}
-                  description={cocktail.description}
+                  avg_rating={cocktail.avg_rating}
                   virginCocktail={cocktail}
+                  cocktail_id={cocktail.cocktail_id}
+                  virginCocktails={virginCocktails}
                 />
               </div>
             ))}
