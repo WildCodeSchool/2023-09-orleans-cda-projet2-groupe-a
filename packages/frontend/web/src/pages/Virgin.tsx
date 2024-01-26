4337f3f58640f9b481e968bee55d24e5eb6e2fb7
Cardamced <120644423+Cardamced@users.noreply.github.com>
2024-01-26 09:39:36 +0100

    feat: virgin
    fix style in process

--- packages/frontend/web/src/pages/Virgin.tsx
+++ packages/frontend/web/src/pages/Virgin.tsx
@@ -41,12 +41,12 @@ export default function VirginCocktails() {
     >
       <div className='flex h-full w-full items-center justify-center'>
         <div className='bg-card-virgin-orange border-light-orange flex h-full w-full justify-center rounded-lg border-4 p-3 shadow-2xl sm:w-[70vw] md:h-[13rem] md:w-[27rem]'>
-          <div className='flex h-full w-full flex-col items-center justify-center'>
-            <h1 className='font-stroke text-light absolute z-50 m-auto mr-[200px] flex stroke-[2rem] py-[4rem] ps-5 text-center text-4xl text-[2.5rem] font-extrabold uppercase'>
+          <div className='flex h-full w-full flex-col items-center justify-center sm:ml-[20px]'>
+            <h1 className='font-stroke md:font-stroke text-light absolute z-50 m-auto mr-[200px] flex stroke-[2rem] py-[4rem] ps-5 text-center text-[2.5rem] font-extrabold uppercase sm:text-2xl md:text-4xl'>
               {'Virgin'}
             </h1>
             <div
-              className='z-50 ml-[200px] h-[200px] w-[350px] bg-center bg-no-repeat'
+              className='z-50 ml-[250px] h-[200px] w-[350px] bg-center bg-no-repeat sm:h-[150px] sm:w-[300px] md:ml-[200px]'
               style={{
                 backgroundImage: `url('/feeding-bottle-cocktails-2.svg')`,
               }}
@@ -55,7 +55,7 @@ export default function VirginCocktails() {
         </div>
       </div>
       <div className='px-10'>
-        <div className=' flex flex-col items-center justify-center gap-y-10 md:my-5 md:grid md:grid-cols-2 md:flex-row lg:grid-cols-3 xl:grid-cols-4 2xl:px-20'>
+        <div className=' 2xl :px-20 flex flex-col items-center justify-center gap-y-10 md:my-5 md:grid md:grid-cols-2 md:flex-row lg:grid-cols-3 xl:grid-cols-4'>
           {virginCocktails.length > 0 &&
             virginCocktails.map((cocktail, index) => (
               <div key={index} className='pb-[22rem]'>
